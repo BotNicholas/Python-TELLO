afef8871fc77681203c1179902478297e00c71ce
BotNicholas <botannicolai22@gmail.com>
2023-03-14 17:54:58 +0200

    Trying to add camera

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 #Camera lesson
-from djitellopy import Tello
-import cv2
-
-tello = Tello()
-tello.connect()
-tello.streamon() #to use camera
-
-while True:
-    img = tello.get_frame_read().frame
-    img = cv2.resize(img, (360, 240))
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+#to finish
 
 
 
