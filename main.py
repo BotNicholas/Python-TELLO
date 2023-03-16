86f1c3af8339f7ba43a21d7bd5071c83add5d3fc
BotNicholas <botannicolai22@gmail.com>
2023-03-16 09:13:48 +0200

    Clean the project and trowing out useless.

--- main.py
+++ main.py
@@ -1,14 +1,172 @@
-#Camera lesson
-#to finish
+#Computer Vision lesson (Camera)
+
+#1-st variant
+# from djitellopy import Tello
+# import cv2
+#
+# tello = Tello()
+# #cap = cv2.VideoCapture(0)
+# tello.connect()
+# print(tello.get_battery())
+# tello.streamon()
+#
+#
+# while True:
+#     img = tello.get_frame_read().frame
+#     img = cv2.resize(img, (360, 240))
+#     cv2.imshow("results", img)
+#     cv2.waitKey(1)      #1 - no one key is not pressed
+
+
+
+#2-nd variant
+from djitellopy import Tello
+import cv2
+
+#Drone initializing
+# tello = Tello()
+# tello.connect()
+# tello.streamon()
+#
+#
+# #initialize OpenCV window
+# cv2.namedWindow("Tello Video Feed")
+#
+# while True:
+#     #get frame from tello
+#     frame = tello.get_frame_read().frame
+#
+#     #displaying the frame on the OpenCV window
+#     cv2.imshow("Tello Video Feed", frame)
+#
+#     #if q key was pressed we have to close the vieo stream
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+#
+# #cleaning
+# cv2.destrotAllWindows()
+# tello.streamoff()
+# tello.end()
+
+
+
+
+
+from djitellopy import Tello
+import pygame
+import cv2
+
+tello = Tello()
+tello.connect()
+
+print(tello.get_battery())
+
+pygame.init()
+win = pygame.display.set_mode((500, 300))
+
+
+#here we are getting key inputs
+def getKey(keyName):
+    ans = False
+    for eve in pygame.event.get():pass      #eve - event
+    keyInput = pygame.key.get_pressed()
+    myKey = getattr(pygame, 'K_{}'.format(keyName))
+    if keyInput[myKey]:
+        ans = True
+
+    pygame.display.update()
+
+    return ans
+
+
+#drone control. Getting key
+def getKeyboardInput():
+    lr,fb, ud, yv  = 0, 0, 0, 0 #left/right    forward/back    up/down  yv (yaw-velocity) - rotation
+
+    #slow
+    #speed = 50
+
+    #normal
+    speed = 100
+
+    #sport
+    #speed = 150
+
+    #ultra
+    #speed = 500
+
+    rotationSpeed = 200
+
+    if getKey("LEFT"): lr = -speed
+    elif getKey("RIGHT"): lr = speed
+
+    if getKey("UP"): fb = speed
+    elif getKey("DOWN"): fb = -speed
+
+    if getKey("w"): ud = speed
+    elif getKey("s"): ud = -speed
+
+    if getKey("a"): yv = -rotationSpeed
+    elif getKey("d"): yv = rotationSpeed
+
+    if getKey("SPACE"): tello.takeoff()
+    elif getKey("l"): tello.land()
+
+
+    if getKey("KP8"):
+        tello.flip_forward()
+    if getKey("KP2"):
+        tello.flip_back()
+    if getKey("KP4"):
+        tello.flip_left()
+    if getKey("KP6"):
+        tello.flip_right()
+
+
+    if getKey("1"):
+        tello.streamon()
+
+
+
+    if getKey("x"):
+        tello.flip_forward()
+        tello.rotate_counter_clockwise(90)
+        tello.flip_left()
+        tello.rotate_counter_clockwise(90)
+        tello.flip_back()
+        tello.rotate_counter_clockwise(90)
+        tello.flip_right()
+        tello.rotate_counter_clockwise(90)
+
+
+
+    return [lr, fb, ud, yv]   #It is like am list, but it is more like an array (string)
+
+
+
+while True:
+    vals = getKeyboardInput()   #it is like an array in pyhon
+    #print(vals)
+    tello.send_rc_control(vals[0], vals[1], vals[2], vals[3]) #this method allows more complicated muvments. It is much better to use than code variant before...
+
+
+
+
+
+
+
+
+
 
 
 
 # import pygame
 # from djitellopy import Tello    #we do not import all the library because it may have the same methods or classes, that other libraries may have
 #
-# myTello = Tello()
-# myTello.connect()
-# print(myTello.get_battery())
+# tello = Tello()
+# tello.connect()
+# print(tello.get_battery())
 #
 # #initializing pygame window
 # # def init():
@@ -63,29 +221,29 @@
 #     if getKey("a"): yv = -rotationSpeed
 #     elif getKey("d"): yv = rotationSpeed
 #
-#     if getKey("SPACE"): myTello.takeoff()
-#     elif getKey("l"): myTello.land()
+#     if getKey("SPACE"): tello.takeoff()
+#     elif getKey("l"): tello.land()
 #
 #
 #     if getKey("KP8"):
-#         myTello.flip_forward()
+#         tello.flip_forward()
 #     if getKey("KP2"):
-#         myTello.flip_back()
+#         tello.flip_back()
 #     if getKey("KP4"):
-#         myTello.flip_left()
+#         tello.flip_left()
 #     if getKey("KP6"):
-#         myTello.flip_right()
+#         tello.flip_right()
 #
 #
 #     if getKey("x"):
-#         myTello.flip_forward()
-#         myTello.rotate_counter_clockwise(90)
-#         myTello.flip_left()
-#         myTello.rotate_counter_clockwise(90)
-#         myTello.flip_back()
-#         myTello.rotate_counter_clockwise(90)
-#         myTello.flip_right()
-#         myTello.rotate_counter_clockwise(90)
+#         tello.flip_forward()
+#         tello.rotate_counter_clockwise(90)
+#         tello.flip_left()
+#         tello.rotate_counter_clockwise(90)
+#         tello.flip_back()
+#         tello.rotate_counter_clockwise(90)
+#         tello.flip_right()
+#         tello.rotate_counter_clockwise(90)
 #
 #
 #
@@ -95,7 +253,7 @@
 # while True:
 #     vals = getKeyboardInput()   #it is like an array in pyhon
 #     print(vals)
-#     myTello.send_rc_control(vals[0], vals[1], vals[2], vals[3]) #this method allows more complicated muvments. It is much better to use than code variant before...
+#     tello.send_rc_control(vals[0], vals[1], vals[2], vals[3]) #this method allows more complicated muvments. It is much better to use than code variant before...
 #
 
 
@@ -144,9 +302,9 @@
 # from time import sleep
 # from djitellopy import Tello    #we do not import all the library because it may ahave the same methods or classess, that other libraries may have
 #
-# myTello = Tello()
-# myTello.connect()
-# print(myTello.get_battery())
+# tello = Tello()
+# tello.connect()
+# print(tello.get_battery())
 #
 # #initializing pygame window
 # def init():
@@ -171,55 +329,55 @@
 #
 #
 #     if getKey("LEFT"):
-#         myTello.move_left(100)
+#         tello.move_left(100)
 #         #print("A left key has been pressed!")
 #
 #     if getKey("RIGHT"):
-#         myTello.move_right(100)
+#         tello.move_right(100)
 #         #print("A right key has been pressed!")
 #
 #     if getKey("UP"):
-#         myTello.move_forward(100)
+#         tello.move_forward(100)
 #         #print("An up key has been pressed!")
 #
 #     if getKey("DOWN"):
-#         myTello.move_back(100)
+#         tello.move_back(100)
 #         #print("A down key has been pressed!")
 #
 #     if getKey("w"):
-#         myTello.move_up(100)
+#         tello.move_up(100)
 #         #print("W key has been pressed!")
 #
 #     if getKey("a"):
-#         myTello.rotate_clockwise(90)
+#         tello.rotate_clockwise(90)
 #         #print("A key has been pressed!")
 #
 #     if getKey("s"):
-#         myTello.move_down(100)
+#         tello.move_down(100)
 #         #print("S key has been pressed!")
 #
 #     if getKey("d"):
-#         myTello.rotate_counter_clockwise(90)
+#         tello.rotate_counter_clockwise(90)
 #         #print("D key has been pressed!")
 #
 #     if getKey("SPACE"):
-#         myTello.takeoff()
+#         tello.takeoff()
 #         #print("A space has been pressed")
 #
 #     if getKey("l"):
-#         myTello.land()
+#         tello.land()
 #
 #     if getKey("KP8"):
-#         myTello.flip_forward()
+#         tello.flip_forward()
 #
 #     if getKey("KP2"):
-#         myTello.flip_back()
+#         tello.flip_back()
 #
 #     if getKey("KP4"):
-#         myTello.flip_left()
+#         tello.flip_left()
 #
 #     if getKey("KP6"):
-#         myTello.flip_right()
+#         tello.flip_right()
 #
 #     #optional... but don't work as it should be ;)
 #     if getKey("ESCAPE"):
